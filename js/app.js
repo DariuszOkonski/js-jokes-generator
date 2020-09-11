5e5cee9a1abdd324ea93755386b2fdfb64b3ce5b
Dariusz Okonski <darek200180@gmail.com>
2020-09-11 13:54:24 +0100

    working on loading

--- js/app.js
+++ js/app.js
@@ -1,9 +1,12 @@
 const domElements = {
     spanText: document.querySelector('[data-text]'),
+    divLoader: document.querySelector('[data-loader]'),
+    divBoard: document.querySelector('[data-loader]'),
     btnTwitter: document.querySelector('[data-btn-twitter]'),
     btnJoke: document.querySelector('[data-btn-joke]')
 }
 
+const textLength = 100;
 const jokeAPI = "https://api.icndb.com/jokes/random";
 
 function addEventsListeners() {
@@ -11,29 +14,61 @@ function addEventsListeners() {
     domElements.btnJoke.addEventListener('click',getNewJoke);
 }
 
-async function getNewJoke() {
-    fetch(jokeAPI)
+function checkJokeLength(joke) {
+    console.log(joke.length);
+
+    if(joke.length > textLength) {
+        domElements.spanText.classList.add('text--small');
+    } else {
+        domElements.spanText.classList.remove('text--small');
+    }
+}
+
+function getNewJoke() {
+    // toggleLoading();
+    // addLoading();
+
+    setTimeout(() => {
+        
+        
+        fetch(jokeAPI)
         .then(resp => {
             if(resp.status === 200)
-                return resp.json();
+            return resp.json();
             
             throw new Error();
         })
         .then(data => {
             const joke = data.value.joke;
-
-            console.log(joke);
+            
+            checkJokeLength(joke);
             domElements.spanText.innerText = joke;
+            // toggleLoading();
         })
         .catch(err => {
             console.log('Something went wrong: ', err.message);
         })
+    }, 2000);
 }
 
 function goToTwitter() {
     const twitterUrl = `https://twitter.com/`;
 
     window.open(twitterUrl, '_blank');
+}
+
+function addLoading() {
+    domElements.divLoader.classList.remove('hidden');
+    domElements.divBoard.classList.add('hidden');
+}
+
+function removeLoading() {
+    console.log('remove loading');
+}
+
+function toggleLoading() {
+    domElements.divLoader.classList.toggle('hidden');
+    domElements.divBoard.classList.toggle('hidden');
 }
 
 // Call funcions
